7ef910b25704a82b2f8a27c37642ec897f970032
Virginie Cazé <virginie.caze.contact.pro@gmail.com>
2020-10-04 18:00:57 +0200

    Réalisation du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bibli';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyBAi2oLjlYmQuX_h2zkuNBxiU9pF1GCkz0",
+      authDomain: "library-866c0.firebaseapp.com",
+      databaseURL: "https://library-866c0.firebaseio.com",
+      projectId: "library-866c0",
+      storageBucket: "library-866c0.appspot.com",
+      messagingSenderId: "308929490085",
+      appId: "1:308929490085:web:b3e73b2fba66d4f9e2f14c"
+    };
+    firebase.initializeApp(config);
+  }
 }
